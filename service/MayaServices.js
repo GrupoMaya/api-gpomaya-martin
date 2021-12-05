70ac1b6c2ffaddb5593e3de9a40ed54aafadd1f1
cchavezmx <cchavezmx@outlook.com>
2021-12-05 04:13:58 -0600

    se mejora la busqueda de clientes

--- service/MayaServices.js
+++ service/MayaServices.js
@@ -6,7 +6,7 @@ module.exports = {
   addProyecto: async (payload) => {
     return new Proyecto(payload).save()
   },
-  getAllClientes: async () => Clientes.find({ isActive: true }),
+  getAllClientes: async () => Clientes.aggregate().match({}).project({ nombre: 1 }),
   // 
   getAllProyectos: async () => {
 
