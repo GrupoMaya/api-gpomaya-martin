acd787cd6598d2e794b6cd67058b2056b76ff4b6
cchavezmx <cchavezmx@outlook.com>
2022-03-14 01:47:23 -0600

    se añade test

--- config/index.js
+++ config/index.js
@@ -1,7 +1,5 @@
 const NODE_ENV = process.env.NODE_ENV
 
-console.log('NODE_ENV:', NODE_ENV)
-
 const config = {
   develop: {
     MONGO_URI: `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_URI_DEV}`
